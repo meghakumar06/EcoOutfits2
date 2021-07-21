29a5e5130e56a8f79f9b269e2d3895dc6fd7e63f
Katherine Dong <dongkatherine3@gmail.com>
2021-07-21 11:42:40 -0400

    Initial Commit

---
+++ EcoOutfits/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EcoOutfits
+//
+//  Created by Katherine Dong on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var openingText: UITextField!
+    @IBOutlet weak var openingImage: UIImageView!
+    @IBOutlet weak var openingDescription: UITextView!
+    @IBOutlet weak var openingAboutUs: UITextField!
+    @IBOutlet weak var openingAboutDescription: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
